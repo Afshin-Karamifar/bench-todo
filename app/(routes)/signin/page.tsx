1b0aca38f9da920d0d24b2ce9a5ebc1463933fd0
Afshin-Karamifar <afshin.karamifar@outlook.com>
2023-09-27 14:37:46 +0100

    Metadata updated

--- app/(routes)/signin/page.tsx
+++ app/(routes)/signin/page.tsx
@@ -2,22 +2,25 @@ import { redirect } from "next/navigation";
 import styles from "../signup/signup.module.scss";
 import SignInForm from "./SignInForm";
 import { GetUserInfo } from "@/app/_utils/GetUserInfo";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
 
 export default async function SignIn() {
-  if (await GetUserInfo()) redirect("/");
+  const user = await GetUserInfo();
+  if (user) redirect("/");
 
   return (
-    <>
-      <title>Sign In</title>
-      <div className="govuk-width-container">
-        <div className="govuk-grid-row govuk-!-static-margin-0">
-          <div
-            className={`${styles.mainContainer} govuk-grid-column-two-thirds govuk-!-static-margin-top-8 govuk-!-static-padding-0`}
-          >
-            <SignInForm />
-          </div>
+    <div className="govuk-width-container">
+      <div className="govuk-grid-row govuk-!-static-margin-0">
+        <div
+          className={`${styles.mainContainer} govuk-grid-column-two-thirds govuk-!-static-margin-top-8 govuk-!-static-padding-0`}
+        >
+          <SignInForm />
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -2,22 +2,25 @@ import SignUpForm from "./SignUpForm";
 import styles from "./signup.module.scss";
 import { redirect } from "next/navigation";
 import { GetUserInfo } from "@/app/_utils/GetUserInfo";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
 
 export default async function SignUp() {
-  if (await GetUserInfo()) redirect("/");
+  const user = await GetUserInfo();
+  if (user) redirect("/");
 
   return (
-    <>
-      <title>Sign Up</title>
-      <div className="govuk-width-container">
-        <div className="govuk-grid-row govuk-!-static-margin-0">
-          <div
-            className={`${styles.mainContainer} govuk-grid-column-two-thirds govuk-!-static-margin-top-8 govuk-!-static-padding-0`}
-          >
-            <SignUpForm />
-          </div>
+    <div className="govuk-width-container">
+      <div className="govuk-grid-row govuk-!-static-margin-0">
+        <div
+          className={`${styles.mainContainer} govuk-grid-column-two-thirds govuk-!-static-margin-top-8 govuk-!-static-padding-0`}
+        >
+          <SignUpForm />
         </div>
       </div>
-    </>
+    </div>
   );
 }
